e1bab401fc25bca9de4dae491f7fe0bbc049eba8
Olivier Lange <olange@hey.com>
2024-05-08 23:07:25 +0200

    feat: enables Vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,25 @@
-import { defineConfig } from 'astro/config';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "astro/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercel(),
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [],
   vite: {
     plugins: [
       viteStaticCopy({
         targets: [
           {
-            src: 'node_modules/@shoelace-style/shoelace/dist/assets/**/*',
-            dest: './shoelace/assets'
-          }
-        ]
-      })
-    ]
-  }
+            src: "node_modules/@shoelace-style/shoelace/dist/assets/**/*",
+            dest: "./shoelace/assets",
+          },
+        ],
+      }),
+    ],
+  },
 });
